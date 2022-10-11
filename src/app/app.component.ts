a8b1616418ef1f2fcade262603fe76301a98ca3b
DiegoBarrosA <diegobarrosaraya@duocuc.cl>
2022-10-11 09:32:37 -0300

    New theme and status-bar color now matches with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor( private statusBar: StatusBar) {
+
+
+// let status bar overlay webview
+this.statusBar.overlaysWebView(false);
+// set status bar to white
+this.statusBar.backgroundColorByHexString('#b0ccff');
+
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, StatusBar  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
